be1165dbec384eb07d09bb6dd3284cefa9f41e36
Matthew Fajardo <fajardomj@live.com>
2013-11-27 00:04:45 -0700

    Server now finds javascript (hardcoded url)
    Angularjs works. Experimenting with changing username

---
+++ SocialNetworking/SocialNetworking/static/js/socialNetwork.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Fajardo on 11/26/13.
+ */
+    'use strict';
+// / declare a module
+var myapp = angular.module('socialNetwork', []);
+
+
+ function LoginController($scope) {
+
+        $scope.login = function() {
+        $scope.username = "Matthew";
+        }
+    }
+
+
